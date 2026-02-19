b94b4f9b54316cb92f0b5b977aecc1f47c4b5440
Phant0M588 <egor2007-13@mail.ru>
2026-02-19 09:08:49 +0300

    Создал новый файл Main4 и написал код

---
+++ src/Main4.java
@@ -0,0 +1,44 @@
+void main()
+{
+    Scanner in = new Scanner (System.in);
+    int q = in.nextInt();
+    int[][] mass = new int [q][q];
+    int[] mas = new int [q];
+    for(int i = 0; i < q; i++)
+    {
+        for(int j = 0; j < q; j++)
+        {
+            int w = in.nextInt();
+            mass[i][j] = w;
+        }
+    }
+    for(int i = 0; i < q; i++)
+    {
+        mas[i] = mass[i][i];
+    }
+    int left = 0;
+    int right = mas.length-1;
+    while(left <= right)
+    {
+        for (int i = right; i > left; i--)
+        {
+            if(mas[i-1] > mas[i])
+            {
+                int temp = mas[i];
+                mas[i] = mas[i - 1];
+                mas[i - 1] = temp;
+            }
+        }
+        left++;
+        for (int i = left; i < right; i++)
+        {
+            if (mas[i] > mas[i+1])
+            {
+                int temp = mas[i + 1];
+                mas[i + 1] = mas[i];
+                mas[i] = temp;
+            }
+        }
+        right--;
+    }
+}
