de5589df5b240d7d04d48d36f990721eef25085c
Phant0M588 <egor2007-13@mail.ru>
2026-03-20 10:08:49 +0300

    Переписал 3 код

--- src/Main5.java
+++ src/Main5.java
@@ -1,33 +1,33 @@
 void main()
 {
-    Rab pri1 = new Rab();
-    Rab pri2 = new Rab();
-    Rab pri3 = new Rab();
-
+    Rab pri = new Rab();
     Scanner in = new Scanner (System.in);
-    pri1.x = in.nextInt();
-    pri1.y = ((3* pri1.x)+5);
-
-    pri2.a = in.nextInt();
-    pri2.b = in.nextInt();
-    pri2.y = ((pri2.a + pri2.b)/(pri2.a - pri2.b));
-
-    pri3.x = in.nextInt();
-    pri3.a = in.nextInt();
-    pri3.b = in.nextInt();
-    pri3.y = ((pri3.a * pri3.x) / pri3.b);
-    int result = 1;
-    for (int i = 1; i <= pri3.y; i++)
-    {
-        result = result * i;
-    }
-    pri3.y = result;
-    System.out.println(pri1.y + " " + pri2.y + " " + pri3.y);
+    pri.x = in.nextInt();
+    pri.a = in.nextInt();
+    pri.b = in.nextInt();
+    System.out.println(pri.first() + " " + pri.second() + " " + pri.third());
 }
 class Rab
 {
     int x;
     int a;
     int b;
-    int y;
+
+    int first(){
+        return 3*x+5;
+    }
+
+    int second(){
+        return (a + b)/(a - b);
+    }
+
+    int third(){
+        int y = ((a * x) / b);
+        int result = 1;
+        for (int i = 1; i <= y; i++)
+        {
+            result = result * i;
+        }
+        return result;
+    }
 }
