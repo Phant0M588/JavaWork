9d768c5bba79ec996b370487021ce15e92c303c5
PhanT0M <egor2007-13@mail.ru>
2026-05-25 16:39:55 +0300

    Написал 14 практическую работу
    Signed-off-by: PhanT0M <egor2007-13@mail.ru>

--- src/Main14.java
+++ src/Main14.java
@@ -1,17 +1,25 @@
-void main() {
-    String cc = "Привет пользователь с IP-адресом 123.234.001.111";
-    String cc1 = "123.234.001.111";
-    Pattern IP = Pattern.compile("(([0 1]\\d\\d | 2[0-4]\\d | 25[0-5])\\.){3}([0 1]\\d\\d | 2[0-4]\\d | 25[0-5])");
-    String IP1 = IP.toString();
-    Matcher M = IP.matcher(cc);
-    boolean res = M.find();
-    if (res) {
-        System.out.println(IP1);
-    } else {
-        System.out.println(IP1 + " не равен " + cc1);
-    }
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class Main14 {
+    public static final String cc = "Привет пользователь с IP-адресом 123.234.001.111";
+    public static final String IP = "(([0 1]\\d\\d|2[0-4]\\d|25[0-5])\\.){3}([0 1]\\d\\d|2[0-4]\\d|25[0-5])";
+
+    void main() {
+        String cc = "Привет пользователь с IP-адресом 123.234.001.111";
+        String cc1 = "123.234.001.111";
+        Pattern IP = Pattern.compile("(([0 1]\\d\\d|2[0-4]\\d|25[0-5])\\.){3}([0 1]\\d\\d|2[0-4]\\d|25[0-5])");
+        Matcher M = IP.matcher(cc);
+        boolean res = M.find();
+        if (res) {
+            System.out.println(IP);
+        } else {
+            System.out.println(IP + " не равен " + cc1);
+        }
+
+    }
 }
+
 
 //Лабораторная работа № 14 по разработке программных модулей.
 //В строке найти и вывести корректный ip-адрес при помощи регулярных выражений
